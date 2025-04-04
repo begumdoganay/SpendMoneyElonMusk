61fb95df10a1739bac02945068d0b2cfa9547622
begüm doğanay <86783970+begumdoganay@users.noreply.github.com>
2025-04-04 19:24:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpendElonMuskMoney from './SpendElonMuskMoney';
+
+function App() {
+  return (
+    <div className="App">
+      <SpendElonMuskMoney />
+    </div>
+  );
+}
+
+export default App;
